67fae0bcc84ccd62271dd977448e174a17c8f511
jasmine <jasminetripti5@gmail.com>
2021-08-22 15:04:04 +0600

    event-onclick

---
+++ js/index.js
@@ -0,0 +1,8 @@
+let btn = document.getElementById('click-me');
+btn.addEventListener('click',function(){
+//     let h1 = document.getElementById('my-h1')
+// h1.innerHTML("This is jasmine Tripti")
+alert('this is me');
+
+
+})
