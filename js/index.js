14ac11d1dc6f95606143b6ad74bdf69e16e7e843
jasmine <jasminetripti5@gmail.com>
2021-08-23 15:32:47 +0600

    update-clickEvent

--- js/index.js
+++ js/index.js
@@ -1,8 +1,29 @@
-let btn = document.getElementById('click-me');
+
+// let p = document.getElementById('para')
+// console.log(p)
+
+// let head = document.getElementsByClassName('header')
+// console.log(head)
+
+// let p1 = document.getElementsByTagName('div')
+// console.log(p1)
+
+//click function
+// let btn = document.getElementById('add-me');
+// btn.addEventListener('click',function(){
+// alert('this is me');
+// })
+
+  
+
+let btn = document.getElementById('add-me');
 btn.addEventListener('click',function(){
-//     let h1 = document.getElementById('my-h1')
-// h1.innerHTML("This is jasmine Tripti")
-alert('this is me');
+    let h1 = document.getElementById('my-h1')
+    h1.innerHTML = ("I am student of bubt")
 
-
+    let p = document.getElementById('para')
+    p.innerHTML = (" I am currently working in html5,css3,js,react")
 })
+
+
+
