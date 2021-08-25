fdebd192f4df11948c92686a01a6e199b187bae6
jasmine <jasminetripti5@gmail.com>
2021-08-25 16:17:25 +0600

    on click, show-hide

--- js/index.js
+++ js/index.js
@@ -36,15 +36,39 @@
 
 
 
-let p = document.querySelector('#para')
-// p.style.background = 'red'
-let p1Styles ={
-color:'orange',
-fontSize:'24px',
-padding:'10px'
+// let p = document.querySelector('#para')
+// // p.style.background = 'red'
+// let p1Styles ={
+// color:'orange',
+// fontSize:'24px',
+// padding:'10px'
+
+// }
+
+// Object.assign(p.style,p1Styles)
+
+
+
+btn = document.querySelector("#btn");
+
+let para = document.querySelector ("#para");
+
+let isShown = false;
+
+btn.addEventListener("click", function() {
+
+if (isShown) {
+para.style.visibility = "hidden";
+isShown = false;
+btn.innerHTML = "Show";
 
 }
+ else {
+para.style.visibility = "visible";
+isShown = true;
+btn.innerHTML = "Hide";
 
-Object.assign(p.style,p1Styles)
+} 
+});
 
 
